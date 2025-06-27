f698e1d5de9f4a482ce67fab1f2442e95f6be2a1
s1320227 <s1320227@u-aizu.ac.jp>
2025-06-27 12:22:19 +0900

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -7,18 +7,19 @@ public class DiceGame {
   public DiceGame(){
   }
   public static void main(String[] args){
+    DiceGame game = new DiceGame();
     Random rand = new Random();
     die1 = rand.nextInt(6) + 1;
     die2 = rand.nextInt(6) + 1;
     int total = die1 + die2;
     
-    Greeting();
+    game.Greeting();
     
     System.out.println("Rolling dice...");
     System.out.println("Die 1: " + die1);
     System.out.println("Die 2: " + die2);
     System.out.println("Total value: " + total);
-    WinLoseChecker(total);
+    game.WinLoseChecker(total);
   }
 
   
