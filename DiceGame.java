0895af9a50bc7537586d383770717ed23b228cb3
s1320227 <s1320227@u-aizu.ac.jp>
2025-06-27 12:11:27 +0900

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -3,6 +3,7 @@ import java.util.Scanner
 
 public class DiceGame {
   private int die1, die2;
+  private String name;
   public DiceGame(){
   }
   public static void main(String[] args){
@@ -21,7 +22,6 @@ public class DiceGame {
   
   public void Greeting(){
     Scanner sc = new Scanner(System.in);
-    String name;
     System.out.println("What is your name?");
     System.out.print("> ");
     name = sc.nextLine();
